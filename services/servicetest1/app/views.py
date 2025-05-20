94b823391b6aaa6f57223157507a87e0eea1d069
Romain <romain.ryckebusch@student.junia.com>
2025-05-20 16:17:45 +0200

    pour toi Khalid

--- services/servicetest1/app/views.py
+++ services/servicetest1/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone   # honours TIME_ZONE and USE_TZ in settings
 
-# Create your views here.
+
+def current_datetime(request):
+    """Return an HTML page with the current date-time."""
+    context = {"now": timezone.localtime()}   # convert to project’s local TZ
+    return render(request, "current_datetime.html", context)
