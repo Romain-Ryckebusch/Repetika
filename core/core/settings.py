b6bf5e77e056084dfc22509ee3abb5e4de84d1db
Romain <mail@mail.com>
2025-05-20 21:05:13 +0200

    renamed servicetest1 to something a bit more explicit

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'servicetest1',
+    'TimeService',
     'MainServer',
 ]
 
--- core/core/urls.py
+++ core/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("servicetest1.urls")),
+    path("", include("TimeService.urls")),
     path("", include("MainServer.urls")),
 ]
 
