fc6014b0a9d32371907bb4e3d30897768d69ac40
LouisDamay <112948472+LouisDamay@users.noreply.github.com>
2025-05-22 11:36:25 +0200

    SettingScreen V0

---
+++ mobileApp/RepetikaApp/src/screens/SettingsScreen.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { View, Text, ScrollView, Switch } from "react-native";
+
+import globalStyles from '../styles/global';
+import colors from '../styles/colors';
+import styles from '../styles/SettingsScreen.style';
+
+import ScreenWrapper from "../components/navigation/screenWrapper";
+import {useTranslation} from "react-i18next";
+
+import { Picker } from '@react-native-picker/picker';
+
+import Settings from '../utils/SettingsFunctions';
+
+export default function StatisticsScreen() {
+
+    const {t}=useTranslation();
+    // Exemple de syntaxe pour utiliser la fonction de traduction ;
+    // {t("homeScreen.splashMessage",{prenom:"Louis"})}
+
+    // Exemple d'état en dur initialisé avec des valeurs quelconques
+    const [settingsValues, setSettingsValues] = useState({
+        "SettingsScreen.section1.param1": true,
+        "SettingsScreen.section1.param2": false,
+        "SettingsScreen.section1.param3": true,
+        "SettingsScreen.section1.param4": false,
+        "SettingsScreen.section1.param5": true,
+        "SettingsScreen.section1.param6": false,
+        "SettingsScreen.section1.param7": true,
+
+        "SettingsScreen.section2.param1": "Français",  // choix
+        "SettingsScreen.section2.param2": true,
+        "SettingsScreen.section2.param3": false,
+    });
+
+    return (
+    <ScreenWrapper scrollable>
+        <View style={{ flex: 1 }}>
+        <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={true} keyboardShouldPersistTaps="always">
+
+            <Text style={globalStyles.title}>{t("SettingsScreen.title")}</Text>
+
+            {Settings.map((section, sectionIndex) => {
+            const [titleRow, ...params] = section;
+
+            return (
+                <View key={`section-${sectionIndex}`} style={styles.section}>
+                <View style={styles.sectionHeader}>
+                    <View style={styles.separator} />
+                    <Text style={styles.sectionTitle}>{t(titleRow[0])}</Text>
+                    <View style={styles.separator} />
+                </View>
+
+                {params.map(([key, type, callback, ...options], index) => (
+                    <View key={key} style={styles.paramLine}>
+                    <Text style={styles.subSectionTitle}>{t(key)}</Text>
+
+                    {type === "boolean" && (
+                        <Switch
+                        trackColor={{ false: colors.grey, true: colors.primary }}
+                        thumbColor={colors.white}
+                        ios_backgroundColor={colors.grey}
+                        onValueChange={(newValue) => {
+                            setSettingsValues(prev => ({ ...prev, [key]: newValue }));
+                            if (callback) callback(newValue);
+                        }}
+                        value={settingsValues[key] || false}
+                        style={{ marginLeft: 'auto' }}
+                        />
+                    )}
+
+                    {type === "choice" && (
+                        <Picker
+                            selectedValue={settingsValues[key] || options[0]}
+                            style={{ width: 150 }}
+                            onValueChange={(itemValue) => {
+                                setSettingsValues(prev => ({ ...prev, [key]: itemValue }));
+                                if (callback) callback(itemValue);
+                            }}
+                        >
+                            {options.map((option) => (
+                                <Picker.Item label={option} value={option} key={option} />
+                            ))}
+                        </Picker>
+                    )}
+                    </View>
+                ))}
+                </View>
+            );
+            })}
+        </ScrollView>
+        </View>
+    </ScreenWrapper>
+    );
+};
--- mobileApp/RepetikaApp/src/screens/StatisticsScreen.js
+++ mobileApp/RepetikaApp/src/screens/StatisticsScreen.js
@@ -1,29 +1,9 @@
-import { View, Text, TouchableOpacity, ScrollView} from "react-native";
+import {View} from "react-native";
 
-import globalStyles from '../styles/global';
-import colors from '../styles/colors';
-import styles from '../styles/SocialScreen.style';
-
-import ScreenWrapper from "../components/navigation/screenWrapper";
-import {useTranslation} from "react-i18next";
-
-
-
-export default function StatisticsScreen() {
-    const {t}=useTranslation();
+export default function SettingsScreen() {
     return (
-        <ScreenWrapper scrollable>
-            <View style={{ flex: 1 }}>
-
-                <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={true} keyboardShouldPersistTaps="always">
-                    <Text style={globalStyles.title}>Stats</Text>
-
-
-
-
-                </ScrollView>
-            </View>
-        </ScreenWrapper>
+        <View>
+            <Text>Paramètres</Text>
+        </View>
     )
 }
-
