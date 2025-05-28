164e9847701744a2d812c28ec881ac2f0319fb6d
Quentin-dePonthaud <quentin.de-ponthaud@student.junia.com>
2025-05-28 13:36:49 +0200

    Update urls.py

--- services/Cours/app/urls.py
+++ services/Cours/app/urls.py
@@ -4,4 +4,5 @@ from .views import GetChapter, GetPDF
 urlpatterns = [
     path('Cours/getChapter/', GetChapter.as_view()),
     path('Cours/getPDF/', GetPDF.as_view()),
+    path('api/ajout-cours/', UploadAPIView.as_view(), name='ajout-cours'),
 ]
