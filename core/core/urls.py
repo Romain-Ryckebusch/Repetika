7761daaf42abedc154adf4676127248ece9dddd4
Quentin-dePonthaud <quentin.de-ponthaud@student.junia.com>
2025-05-28 13:40:55 +0200

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("TimeService.urls")),
-    path("", include("MainServer.urls")),
+    path("", include("MainServer.app.urls")),
+    path("", include("TimeService.app.urls")),
+    path("", include("Cours.app.urls")),
 ]
 
