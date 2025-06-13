d681b961ae022fcded20799b9a799671f68cafb8
LouisDamay <112948472+LouisDamay@users.noreply.github.com>
2025-06-13 10:43:48 +0200

    Update core for Auth system

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path("", include("MainServer.urls")),
@@ -8,5 +10,8 @@ urlpatterns = [
     path("api/planning/", include("Planning.urls")),
     path("api/learning-session/", include("SeanceApprentissage.urls")),
     path("api/main/", include("MainServer.urls")),
+    path("api/auth/", include("Authentification.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
