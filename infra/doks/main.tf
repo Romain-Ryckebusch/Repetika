38dd44b752224e12837b8b227053b5ea89672539
Romain <romain.ryckebusch@student.junia.com>
2026-01-23 23:34:05 +0100

    feat(infra): initial do kubernetes deployment via terraform and cd workflow

---
+++ infra/doks/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+  }
+}
+
+########################
+# Module inputs
+########################
+
+variable "namespace" {
+  type = string
+}
+
+variable "image_registry" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "django_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "postgres_password" {
+  type      = string
+  sensitive = true
+}
+
+########################
+# Resources
+########################
+
+resource "kubernetes_namespace" "repetika" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "helm_release" "repetika" {
+  name      = "repetika"
+  namespace = kubernetes_namespace.repetika.metadata[0].name
+
+  chart = "${path.root}/charts/repetika"
+
+  values = [
+    file("${path.root}/charts/repetika/values.yaml")
+  ]
+
+  set {
+    name  = "global.namespace"
+    value = var.namespace
+  }
+
+  set {
+    name  = "image.registry"
+    value = var.image_registry
+  }
+
+  set {
+    name  = "image.tag"
+    value = var.image_tag
+  }
+
+  set {
+    name  = "image.pullPolicy"
+    value = "IfNotPresent"
+  }
+
+  set_sensitive {
+    name  = "global.app.secretKey"
+    value = var.django_secret_key
+  }
+
+  set_sensitive {
+    name  = "global.postgres.password"
+    value = var.postgres_password
+  }
+
+  timeout = 600
+}
---
+++ main.tf
@@ -0,0 +1,108 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "repetika"
+    workspaces {
+      name = "repetika-doks"
+    }
+  }
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+  }
+}
+
+########################
+# Root-level variables
+########################
+
+variable "do_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "image_registry" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "django_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "postgres_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "namespace" {
+  type    = string
+  default = "repetika"
+}
+
+########################
+# Providers + cluster
+########################
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_kubernetes_cluster" "this" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host = data.digitalocean_kubernetes_cluster.this.endpoint
+
+  token                  = data.digitalocean_kubernetes_cluster.this.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.digitalocean_kubernetes_cluster.this.endpoint
+
+    token                  = data.digitalocean_kubernetes_cluster.this.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      data.digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
+
+########################
+# DOKS app module
+########################
+
+module "doks" {
+  source = "./infra/doks"
+
+  namespace          = var.namespace
+  image_registry     = var.image_registry
+  image_tag          = var.image_tag
+  django_secret_key  = var.django_secret_key
+  postgres_password  = var.postgres_password
+}
